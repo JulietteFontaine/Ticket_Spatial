d4f9e278448b4ea9c785d83525fac94c64dd34da
Juliette Fontaine <35955834+JulietteFontaine@users.noreply.github.com>
2020-09-24 13:10:24 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -16,34 +16,34 @@ router.get('/', function(req, res, next) {
 
 
 // Remplissage de la base de donnée, une fois suffit
-router.get('/save', async function(req, res, next) {
+// router.get('/save', async function(req, res, next) {
 
-  // How many journeys we want
-  var count = 300
+//   // How many journeys we want
+//   var count = 300
 
-  // Save  ---------------------------------------------------
-    for(var i = 0; i< count; i++){
+//   // Save  ---------------------------------------------------
+//     for(var i = 0; i< count; i++){
 
-    departureCity = city[Math.floor(Math.random() * Math.floor(city.length))]
-    arrivalCity = city[Math.floor(Math.random() * Math.floor(city.length))]
+//     departureCity = city[Math.floor(Math.random() * Math.floor(city.length))]
+//     arrivalCity = city[Math.floor(Math.random() * Math.floor(city.length))]
 
-    if(departureCity != arrivalCity){
+//     if(departureCity != arrivalCity){
 
-      var newUser = new journeyModel ({
-        departure: departureCity , 
-        arrival: arrivalCity, 
-        date: date[Math.floor(Math.random() * Math.floor(date.length))],
-        departureTime:Math.floor(Math.random() * Math.floor(23)) + ":00",
-        price: Math.floor(Math.random() * Math.floor(125)) + 25,
-      });
+//       var newUser = new journeyModel ({
+//         departure: departureCity , 
+//         arrival: arrivalCity, 
+//         date: date[Math.floor(Math.random() * Math.floor(date.length))],
+//         departureTime:Math.floor(Math.random() * Math.floor(23)) + ":00",
+//         price: Math.floor(Math.random() * Math.floor(125)) + 25,
+//       });
        
-       await newUser.save();
+//        await newUser.save();
 
-    }
+//     }
 
-  }
-  res.render('index', { title: 'Express' });
-});
+//   }
+//   res.render('index', { title: 'Express' });
+// });
 
 
 // Cette route est juste une verification du Save.
